2abafef9cda808b67f3e9f9ecc4028604de555a8
3arpbl3y <3arpbl3y@gmail.com>
2022-05-02 22:37:26 +0500

    finished, but need to polish with multiple dates

--- script.js
+++ script.js
@@ -1,18 +1,25 @@
-// .days-container {
-// }
-// .hours-container {
-// }
-// .mins-container {
-// }
-// .seconds-container {
-// }
+const daysEl = document.getElementById("days");
+const hoursEl = document.getElementById("hours");
+const minsEl = document.getElementById("mins");
+const secondsEl = document.getElementById("seconds");
 
 const newYear = "1 Jan 2023";
 
 function countdown() {
   const newYearDate = new Date(newYear);
   const currentDate = new Date();
-  console.log(newYearDate - currentDate);
+  const totalSec = (newYearDate - currentDate) / 1000;
+  const mins = Math.floor((totalSec / 60) % 24) % 60;
+  const hours = Math.floor(totalSec / 3600) % 24;
+  const days = Math.floor(totalSec / 3600 / 24);
+  const seconds = Math.floor(totalSec) % 60;
+
+  daysEl.innerHTML = days;
+  hoursEl.innerHTML = hours;
+  minsEl.innerHTML = mins;
+  secondsEl.innerHTML = seconds;
 }
 
 countdown();
+
+setInterval(countdown, 1000);
